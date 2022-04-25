e2711bfb0bc37aa7a16dfbc5ad74980f4c37bbc5
Sangram <sangram15.behera@gmail.com>
2022-04-26 00:44:34 +0530

    Added By Sangram

---
+++ Basic Program/EvenOdd.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace BasicPrograms
+{
+    public class EvenOdd
+    {
+        public void checkEvenOdd()
+        {
+            Console.WriteLine("Enter Number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Even number");
+            }
+            else
+            {
+                Console.WriteLine("Odd number");
+            }
+        }
+    }
+}
