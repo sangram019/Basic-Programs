f9c574575d93dc10861ebe7d98e33ce5936d1015
Sangram <sangram15.behera@gmail.com>
2022-04-26 01:46:03 +0530

    Added By Sangram

---
+++ Basic Program/VowelConsonant.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace BasicPrograms
+{
+    public class VowelConsonant
+    {
+        public void vowel()
+        {
+            char ch;
+            Console.Write("Input an Alphabet (A-Z or a-z) : ");
+            ch = Convert.ToChar(Console.ReadLine().ToLower());
+            int i = ch;
+            if (i >= 48 && i <= 57)
+            {
+                Console.Write("You entered a number, Please enter an alpahbet.");
+            }
+            else
+            {
+                switch (ch)
+                {
+                    case 'a':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    case 'i':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    case 'o':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    case 'u':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    case 'e':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    default:
+                        Console.WriteLine("The Alphabet is not a vowel");
+                        break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
