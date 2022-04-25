c25b15426a7bd96f54a140bbdb09786229067879
Sangram <sangram15.behera@gmail.com>
2022-04-26 00:38:53 +0530

    Added By Sangram

---
+++ Basic Program/LeapYear.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace BasicPrograms
+{
+    public class LeapYear
+    {
+        public void checkLeapYear()
+        {
+            Console.WriteLine("Enter Year");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if ((year % 4 == 0) && (year % 100 != 0))
+            {
+                Console.WriteLine("Leap Year");
+            }
+            else
+            {
+                Console.WriteLine("Not a Leap Year");
+            }
+        }
+    }
+}
