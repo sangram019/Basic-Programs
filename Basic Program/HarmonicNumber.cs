695b87a12032e7a25efe1defc13bfbffc0a9a3cc
Sangram <sangram15.behera@gmail.com>
2022-04-26 01:28:00 +0530

    Added By Sangram

---
+++ Basic Program/HarmonicNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace BasicPrograms
+{
+    public class HarmonicNumber
+    {
+        public void harmonicNumber()
+        {
+            int i, n;
+            double s = 0.0;
+            Console.Write("Input the number of terms : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= n; i++)
+            {
+                Console.Write("1/{0} + ", i);
+                s += 1 / (float)i;
+            }
+            Console.Write("\nSum of Series upto {0} terms : {1} \n", n, s);
+        }
+    }
+
+}
