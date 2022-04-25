7cf2d1f52f5c3aab70cb8e6a5e67c36c4bb97c75
Sangram <sangram15.behera@gmail.com>
2022-04-26 01:35:26 +0530

    Added By Sangram

---
+++ Basic Program/QuotientRemainder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace BasicPrograms
+{
+	public class QuotientRemainder
+	{
+		public void quotient()
+		{
+			int x, y, q, r;
+			Console.Write("Enter first number:");
+			x = Convert.ToInt32(Console.ReadLine());
+
+			Console.Write("Enter second number:");
+			y = Convert.ToInt32(Console.ReadLine());
+
+			q = x / y;
+			r = x % y;
+
+			Console.WriteLine("Quotient is:" + q);
+			Console.WriteLine("Remainder is:" + r);
+
+		}
+	}
+}
