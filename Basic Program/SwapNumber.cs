bebb79e01f2e395fd084575311b6c2ebc4b89de0
Sangram <sangram15.behera@gmail.com>
2022-04-26 01:38:58 +0530

    Added By Sangram

---
+++ Basic Program/SwapNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace BasicPrograms
+{
+    public class SwapNumber
+    {
+        public void swap()
+        {
+            int a, b, c;
+            Console.WriteLine("Enter the First Number : ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Second Number : ");
+            b = int.Parse(Console.ReadLine());
+            c = a;
+            a = b;
+            b = c;
+            Console.WriteLine("After Swapping : ");
+            Console.WriteLine("First Number : " + a);
+            Console.WriteLine("Second Number : " + b);
+            Console.Read();
+        }
+    }
+}
