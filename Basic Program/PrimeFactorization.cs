d51d6d8a3183668f79c18dbf79b2482b98b2653f
Sangram <sangram15.behera@gmail.com>
2022-05-05 00:40:27 +0530

    Added By Sangram

---
+++ Basic Program/PrimeFactorization.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace BasicPrograms
+{
+    public class PrimeFactorization
+    {
+        public void primeFactors()
+        {
+            int a, b;
+            Console.WriteLine("Please enter number: ");
+            a = int.Parse(Console.ReadLine());
+            for (b = 1; b <= a; b++)
+            {
+                if (a % b == 0)
+                {
+                    Console.WriteLine(b + " is a factor of " + a);
+                }
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
